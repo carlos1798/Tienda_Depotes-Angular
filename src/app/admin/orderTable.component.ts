74d1d2931f537d48ae7d49befe4733a725758603
carlos1799 <carlosepila98@gmail.com>
2022-12-17 19:50:12 +0100

    empezar con admin

---
+++ src/app/admin/orderTable.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    template: `
+    <h3 style="padding-top:10px">
+        Tabla de ordenes
+    </h3>`
+})
+export class OrderTableComponent{}
---
+++ src/app/admin/productTable.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    template: `
+    <h3 style="padding-top: 10px">
+        Tablas de producto
+</h3>
+    `
+})
+export class ProductTableComponent{}
